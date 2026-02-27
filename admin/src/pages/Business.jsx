41ccfcc8e1357a120c7fec73fc8b1cd6d86e5ab2
sinhagouravv <sinhagouravv@gmail.com>
2026-02-27 11:14:14 +0530

    updated business temp

--- admin/src/pages/Business.jsx
+++ admin/src/pages/Business.jsx
@@ -5,7 +5,7 @@ const Business = () => {
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-6 text-[#052558]">Business Management</h1>
             <div className="bg-white rounded-xl shadow-sm border border-[#e6f0fa] p-8 text-center text-gray-500">
-                <p>Business module is currently under development.</p>
+                <p>Business module is currently under development</p>
             </div>
         </div>
     );
