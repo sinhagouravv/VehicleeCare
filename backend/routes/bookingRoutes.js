231b6d3dbe3b36adbb118bfec05743fe9614a84c
sinhagouravv <sinhagouravv@gmail.com>
2026-03-31 22:12:00 +0530

    Fixed

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createBooking, getBookings, deleteBooking, getGarageBookings, updateBookingStatus } = require('../controllers/bookingController');
+const { createBooking, getBookings, deleteBooking, getGarageBookings, updateBookingStatus, sendInServiceOTP, verifyInServiceOTP, sendDeliveryOTP, verifyDeliveryOTP } = require('../controllers/bookingController');
 
 router.post('/', createBooking);
 router.get('/', getBookings);
@@ -8,6 +8,10 @@ router.get('/user/:userId', require('../controllers/bookingController').getUserB
 router.get('/garage/:garageId', getGarageBookings);
 router.get('/employee/:employeeId', require('../controllers/bookingController').getEmployeeBookings);
 router.put('/:id/status', updateBookingStatus);
+router.post('/:id/send-otp', sendInServiceOTP);
+router.post('/:id/verify-otp', verifyInServiceOTP);
+router.post('/:id/send-delivery-otp', sendDeliveryOTP);
+router.post('/:id/verify-delivery-otp', verifyDeliveryOTP);
 router.delete('/:id', deleteBooking);
 
 module.exports = router;
