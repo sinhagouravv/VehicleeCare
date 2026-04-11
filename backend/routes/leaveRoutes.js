d290858b921d287390ee71b2e095ea7cf08acdac
sinhagouravv <sinhagouravv@gmail.com>
2026-04-11 23:50:22 +0530

    Updated

--- backend/routes/leaveRoutes.js
+++ backend/routes/leaveRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { requestLeave, getEmployeeLeaves, getAllLeaves, updateLeaveStatus } = require('../controllers/leaveController');
+const { requestLeave, getEmployeeLeaves, getAllLeaves, updateLeaveStatus, getGarageLeaves } = require('../controllers/leaveController');
 
 router.post('/request', requestLeave);
 router.get('/employee/:employeeId', getEmployeeLeaves);
+router.get('/garage/:garageId', getGarageLeaves);
 router.get('/', getAllLeaves);
 router.patch('/:id/status', updateLeaveStatus);
 
