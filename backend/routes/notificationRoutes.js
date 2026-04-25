96d9963b47b75e4b0da2948bb2a28399030b5fc8
sinhagouravv <sinhagouravv@gmail.com>
2026-04-25 23:06:52 +0530

    Updated

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -4,7 +4,9 @@ const ctrl = require('../controllers/notificationController');
 
 router.get('/', ctrl.getAll);
 router.get('/user/:userId', ctrl.getUserNotifications);
+router.post('/create', ctrl.create);
 router.patch('/mark-all-read', ctrl.markAllRead);
+
 router.patch('/:id/read', ctrl.markRead);
 router.delete('/:id', ctrl.deleteOne);
 
