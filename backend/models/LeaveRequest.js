6c067e89b1ae5fe06380ac2319dc87ff430cc779
sinhagouravv <sinhagouravv@gmail.com>
2026-05-29 22:22:59 +0530

    Fixed

--- backend/models/LeaveRequest.js
+++ backend/models/LeaveRequest.js
@@ -56,6 +56,10 @@ const LeaveRequestSchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    remarks: {
+        type: String,
+        required: false
+    },
     leaveId: {
         type: String,
         required: true,
