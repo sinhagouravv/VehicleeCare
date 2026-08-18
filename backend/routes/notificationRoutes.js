f85ea996a2e51265ca760d680cb4b32e53f03590
sinhagouravv <sinhagouravv@gmail.com>
2026-08-18 23:56:31 +0530

    Fixed

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -8,6 +8,7 @@ router.post('/create', ctrl.create);
 router.patch('/mark-all-read', ctrl.markAllRead);
 
 router.patch('/:id/read', ctrl.markRead);
+router.patch('/:id/star', ctrl.toggleStar);
 router.delete('/:id', ctrl.deleteOne);
 
 module.exports = router;
